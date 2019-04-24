2eddf2519540aad3032f2a839f794fd33bce2566
1464770973 <1464770973@qq.com>
2019-04-24 16:07:45 +0800

    add blockRecent file

---
+++ view/blockRecent.js
@@ -0,0 +1,36 @@
+var app = new Vue({
+    el:'#app',
+    data:{
+        recentBlocks:[]
+        
+    },
+    computed: {
+        showRecentBlocks(){
+            var now = Date.now();
+            this.recentBlocks.forEach(block => {
+                block.showtime = parseInt((now-block.time)/1000/60);
+                block.showSizeOnDisk = block.sizeOnDisk.toLocaleString('en');
+                
+            });
+            return this.recentBlocks;
+        }
+        
+    },
+    mounted () {
+        console.log("view mounted");
+        this.getBlockRecent();
+        
+    },
+    methods: {
+        getBlockRecent(){
+            axios.get('http://localhost:8080/block/getRecentBlocks')
+            .then(function(response){
+                console.log(response);
+                app.recentBlocks = response.data;
+            })
+            .catch(function(error){
+                console.log(error);
+            });
+        }
+    }
+})
