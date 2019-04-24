7aa92ba16566347c32530248b6d592eb0afa51b0
1464770973 <1464770973@qq.com>
2019-04-24 21:24:25 +0800

    add byheight file

---
+++ view/blockByHeight.js
@@ -0,0 +1,31 @@
+var app = new Vue({
+    el:'#app',
+    data:{
+        showBlockHeight:[],
+        blockheight:'',
+        
+    },
+    mounted () {
+        console.log("view mounted");
+        var url = new URL(location.href);
+        var height = url.searchParams.get("blockheight");
+        this.getBlockByHeight(height);
+    },
+    methods: {
+        getBlockByHeight(height){
+            axios.get('http://localhost:8080/block/getBlockDetailByHeight',{
+                params:{
+                    blockheight:height
+                }
+            })
+            .then(function(response){
+                console.log(response);
+                app.showBlockHeight = response.data;
+            })
+            .catch(function(error){
+                console.log(error);
+            });
+        }
+        
+    }
+})
